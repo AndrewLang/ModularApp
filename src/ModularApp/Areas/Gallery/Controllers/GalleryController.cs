536b894790fc902407b56f12fd8611372ffaa907
AndrewLang <nnlyx@hotmail.com>
2015-12-29 16:49:11 -0700

    Add gallery model project

--- src/ModularApp/Areas/Gallery/Controllers/GalleryController.cs
+++ src/ModularApp/Areas/Gallery/Controllers/GalleryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Gallery.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +15,7 @@ namespace Gallery.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+            var gallery = new Gallery.Models.Gallery();
             return View();
         }
     }
